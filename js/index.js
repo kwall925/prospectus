f338067fdaa2562ea716f905f0dcffd395ddfc04
Ken <kwall925@gmail.com>
2021-04-02 20:55:42 -0300

    got some functionality, need to fix delete function

--- js/index.js
+++ js/index.js
@@ -1,16 +1,44 @@
 const init = () => {
+  //enable form functionality
   const firstInput = document.querySelector('.addGoal');
   firstInput.onclick = addGoal;
+  //enable functionality for closing form
+  const closer = document.querySelector('.close');
+  closer.onclick = close;
 }
 
+
+//YOUVE GOTTA PAY ATTENTION TO THE ORDER HERE I THINK THATS WHY DELETE FUNCTION NOT WORKING
+
 const addGoal = () => {
-  const goalTitle = document.getElementById('goalTitle');
-  const userText = document.getElementById('userInp').value;
-  document.querySelector('nav').style.opacity = 0.2;
-  // document.querySelector('#wrapper').style.opacity = 0.2;
-  goalTitle.innerHTML = userText;
+  const userText = document.getElementById('userInp');
+  if (userText.value.length > 0){
+    const goalTitle = document.getElementById('goalTitle');
+    document.querySelector('nav').style.opacity = 0.2;
+    // document.querySelector('#wrapper').style.opacity = 0.2;
+    goalTitle.innerHTML = userText.value;
+    const goalForm = document.querySelector('.goalForm');
+    goalForm.style.visibility = 'visible';
+    const goalsList = document.getElementById('newGoal');
+    goalsList.insertAdjacentHTML('afterend', `<div id='list' <li class='li'>${userText.value}</li><img src='images/trash.svg' id='trash'></div>`);
+    //enable functionality for deleting goal
+    const trash = document.getElementById('trash');
+    trash.onclick = deleteItem;
+  } else window.alert("no text bitch");
+  userText.value = '';
+}
+
+const close = () => {
   const goalForm = document.querySelector('.goalForm');
-  goalForm.style.visibility = 'visible';
+  goalForm.style.visibility = 'hidden';
+  document.querySelector('nav').style.opacity = 1;
+}
+
+const deleteItem = () => {
+  const li = document.querySelectorAll('.li');
+  for(let i = 0; i < li.length; i++){
+    li[i].innerHTML = '';
+  }
 }
 
 window.onload = init;
